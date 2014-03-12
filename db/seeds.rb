d0659a4c91ca9916d0615380b79b001e9c57f2bc
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2014-03-12 15:14:52 +0200

    Fixed bug in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,4 @@ lookup = {
     "Pulse".downcase => "5087AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 } 
 
-Concept.find_by_sql("SELECT n.name AS short_name, c.uuid FROM concept c " + 
-        "LEFT OUTER JOIN concept_name n ON n.concept_id = c.concept_id " + 
-        "WHERE name IN ('Respiratory rate','Temperature (C)','SYSTOLIC BLOOD " + 
-        "PRESSURE','DIASTOLIC BLOOD PRESSURE','Weight (kg)','Height (cm)'," + 
-        "'Blood oxygen saturation','Pulse')").each{|c| 
-            c.update_attributes(:uuid => lookup[c.short_name.downcase])
-            
-            puts "Updated uuid for #{c.short_name} to #{c.uuid}"
-        }
+Concept.find_by_sql("SELECT c.concept_id, n.name AS short_name, c.uuid FROM concept c LEFT OUTER JOIN concept_name n ON n.concept_id = c.concept_id WHERE name IN ('Respiratory rate','Temperature (C)','SYSTOLIC BLOOD PRESSURE','DIASTOLIC BLOOD PRESSURE','Weight (kg)','Height (cm)','Blood oxygen saturation','Pulse')").each{|c| concept = Concept.find(c.concept_id); concept.update_attributes(:uuid => lookup[c.short_name.downcase]); puts "Updated uuid for #{c.short_name} to #{concept.uuid}" }
