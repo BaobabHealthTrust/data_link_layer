594db8f1e8cc9786f0b8340ebcce99568bcffe0b
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2014-03-12 14:12:40 +0200

    Added modification of some concept uuids to match newer versions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+lookup = {
+    "Respiratory rate".downcase => "5242AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "Temperature (C)".downcase => "5088AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "SYSTOLIC BLOOD PRESSURE".downcase => "5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "DIASTOLIC BLOOD PRESSURE".downcase => "5086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "Weight (kg)".downcase => "5089AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "Height (cm)".downcase => "5090AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "Blood oxygen saturation".downcase => "5092AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "Pulse".downcase => "5087AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+} 
+
+Concept.find_by_sql("SELECT n.name AS short_name, c.uuid FROM concept c " + 
+        "LEFT OUTER JOIN concept_name n ON n.concept_id = c.concept_id " + 
+        "WHERE name IN ('Respiratory rate','Temperature (C)','SYSTOLIC BLOOD " + 
+        "PRESSURE','DIASTOLIC BLOOD PRESSURE','Weight (kg)','Height (cm)'," + 
+        "'Blood oxygen saturation','Pulse')").each{|c| 
+            c.update_attributes(:uuid => lookup[c.short_name.downcase])
+            
+            puts "Updated uuid for #{c.short_name} to #{c.uuid}"
+        }
