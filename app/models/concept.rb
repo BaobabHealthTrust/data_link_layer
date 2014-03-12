7911df1d463015976cf7c868bc8c7e8cc22f1d19
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2014-03-12 14:12:02 +0200

    Re-enabled concept_id as primary key in concept model

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,6 +1,6 @@
 class Concept < ActiveRecord::Base
   set_table_name "concept"
-  set_primary_key "uuid"
+  set_primary_key "concept_id"
   
   def names
     ConceptName.find(:all, :conditions => ["concept_id = ?", self.concept_id]) rescue []
